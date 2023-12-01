33c767297fbc02ba708e10d6fec47f065bef9561
James <jpnock@gmail.com>
2023-12-01 13:56:27 +0000

    day01: Implement part 2

--- cmd/day01/calibration.go
+++ cmd/day01/calibration.go
@@ -5,15 +5,49 @@ import (
 	"io"
 	"log/slog"
 	"os"
+	"strings"
 )
 
-func outerPositiveSingleDigits(line string) (firstNum, lastNum *int) {
+func firstWordDigitInInput(line string) *int {
+	var wordDigits = map[string]int{
+		"one": 1, "two": 2, "three": 3,
+		"four": 4, "five": 5, "six": 6,
+		"seven": 7, "eight": 8, "nine": 9,
+	}
+
+	// TODO: improve brute force method
+	for wordDigit, val := range wordDigits {
+		if strings.Contains(line, wordDigit) {
+			return &val
+		}
+	}
+
+	return nil
+}
+
+func outerPositiveSingleDigits(line string, includeWordDigits bool) (firstNum, lastNum *int) {
+	accumulatedInput := ""
 	target := &firstNum
 	for _, c := range line {
 		if c >= '0' && c <= '9' {
 			numberFound := int(c - '0')
 			*target = &numberFound
 			target = &lastNum
+			accumulatedInput = ""
+			continue
+		}
+
+		if !includeWordDigits {
+			continue
+		}
+
+		// TODO: tidy
+		accumulatedInput += string(c)
+		if wordDigit := firstWordDigitInInput(accumulatedInput); wordDigit != nil {
+			*target = wordDigit
+			target = &lastNum
+			accumulatedInput = ""
+			continue
 		}
 	}
 	if firstNum != nil && lastNum == nil {
@@ -23,27 +57,32 @@ func outerPositiveSingleDigits(line string) (firstNum, lastNum *int) {
 	return
 }
 
-func generateAlignmentValue(r io.Reader) int {
-	reader := bufio.NewScanner(r)
-
-	alignmentValue := 0
-	for reader.Scan() {
-		line := reader.Text()
-		firstNum, lastNum := outerPositiveSingleDigits(line)
-		if firstNum == nil {
-			continue
-		}
-
-		const tensUnitMultiplier = 10
-		slog.Debug("got vals", "first", *firstNum, "last", *lastNum)
-		alignmentValue += *firstNum * tensUnitMultiplier
-		alignmentValue += *lastNum
+func alignmentValueForLine(line string, includeWordDigits bool) int {
+	firstNum, lastNum := outerPositiveSingleDigits(line, includeWordDigits)
+	if firstNum == nil {
+		return 0
 	}
+
+	const tensUnitMultiplier = 10
+	slog.Debug("got vals", "first", *firstNum, "last", *lastNum)
+	alignmentValue := *firstNum * tensUnitMultiplier
+	alignmentValue += *lastNum
 	return alignmentValue
 }
 
+func generateAlignmentValue(r io.Reader) (withoutWordDigits int, withWordDigits int) {
+	reader := bufio.NewScanner(r)
+
+	for reader.Scan() {
+		line := reader.Text()
+		withoutWordDigits += alignmentValueForLine(line, false)
+		withWordDigits += alignmentValueForLine(line, true)
+	}
+	return
+}
+
 func main() {
-	// Part 1
-	alignmentValue := generateAlignmentValue(os.Stdin)
-	slog.Info("Got alignment", "value", alignmentValue)
+	alignmentValuePart1, alignmentValuePart2 := generateAlignmentValue(os.Stdin)
+	slog.Info("Part 1: got alignment w/o word digits", "value", alignmentValuePart1)
+	slog.Info("Part 2: got alignment w/ word digits", "value", alignmentValuePart2)
 }
--- cmd/day01/calibration_test.go
+++ cmd/day01/calibration_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"strings"
 	"testing"
 
@@ -12,7 +11,7 @@ func newInt(val int) *int {
 	return &val
 }
 
-func Test_outerNaturalNumbersInLine(t *testing.T) {
+func Test_outerPositiveSingleDigits_no_word_digits(t *testing.T) {
 	tests := []struct {
 		name         string
 		line         string
@@ -58,7 +57,82 @@ func Test_outerNaturalNumbersInLine(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotFirstNum, gotLastNum := outerPositiveSingleDigits(tt.line)
+			gotFirstNum, gotLastNum := outerPositiveSingleDigits(tt.line, false)
+			if diff := deep.Equal(gotFirstNum, tt.wantFirstNum); diff != nil {
+				t.Errorf("outerNaturalNumbersInLine() gotFirstNum = %v, want %v", gotFirstNum, tt.wantFirstNum)
+			}
+			if diff := deep.Equal(gotLastNum, tt.wantLastNum); diff != nil {
+				t.Errorf("outerNaturalNumbersInLine() gotLastNum = %v, want %v", gotLastNum, tt.wantLastNum)
+			}
+		})
+	}
+}
+
+func Test_outerPositiveSingleDigits_with_word_digits(t *testing.T) {
+	tests := []struct {
+		name         string
+		line         string
+		wantFirstNum *int
+		wantLastNum  *int
+	}{
+		{
+			name:         "exactly two - 1",
+			line:         "asadsd1dasd3dasd",
+			wantFirstNum: newInt(1),
+			wantLastNum:  newInt(3),
+		},
+		{
+			name:         "exactly two - 2",
+			line:         "2asadsdasddasd5",
+			wantFirstNum: newInt(2),
+			wantLastNum:  newInt(5),
+		},
+		{
+			name:         "exactly two - 3",
+			line:         "2asadsdasd5dasdonea",
+			wantFirstNum: newInt(2),
+			wantLastNum:  newInt(1),
+		},
+		{
+			name:         "exactly two - 4",
+			line:         "asadthree2sdasd5dasdonea",
+			wantFirstNum: newInt(3),
+			wantLastNum:  newInt(1),
+		},
+		{
+			name:         "exactly two - 4",
+			line:         "three2sdasd5dasdonea",
+			wantFirstNum: newInt(3),
+			wantLastNum:  newInt(1),
+		},
+		{
+			name:         "one num - 1",
+			line:         "asdjas3djasdh",
+			wantFirstNum: newInt(3),
+			wantLastNum:  newInt(3),
+		},
+		{
+			name:         "one num - 2",
+			line:         "4",
+			wantFirstNum: newInt(4),
+			wantLastNum:  newInt(4),
+		},
+		{
+			name:         "no nums",
+			line:         "asdjasdjasdh",
+			wantFirstNum: nil,
+			wantLastNum:  nil,
+		},
+		{
+			name:         "empty line",
+			line:         "",
+			wantFirstNum: nil,
+			wantLastNum:  nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotFirstNum, gotLastNum := outerPositiveSingleDigits(tt.line, true)
 			if diff := deep.Equal(gotFirstNum, tt.wantFirstNum); diff != nil {
 				t.Errorf("outerNaturalNumbersInLine() gotFirstNum = %v, want %v", gotFirstNum, tt.wantFirstNum)
 			}
@@ -70,32 +144,31 @@ func Test_outerNaturalNumbersInLine(t *testing.T) {
 }
 
 func Test_generateAlignmentValue(t *testing.T) {
-	const example = `1abc2
+	t.Run("Sample input - no word digits", func(t *testing.T) {
+		const want = 142
+		const example = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`
+		r := strings.NewReader(example)
 
-	type args struct {
-		r io.Reader
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{
-			name: "Sample input",
-			args: args{
-				r: strings.NewReader(example),
-			},
-			want: 142,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := generateAlignmentValue(tt.args.r); got != tt.want {
-				t.Errorf("generateAlignmentValue() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+		if got, _ := generateAlignmentValue(r); got != want {
+			t.Errorf("generateAlignmentValue() = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Sample input - with word digits", func(t *testing.T) {
+		const want = 281
+		const example = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+		r := strings.NewReader(example)
+		if _, got := generateAlignmentValue(r); got != want {
+			t.Errorf("generateAlignmentValue() = %v, want %v", got, want)
+		}
+	})
 }
